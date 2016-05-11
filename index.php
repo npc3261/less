630b6a95e623429147746ac123214e88ef744243
npc3261 <veteranglow@gmail.com>
2016-05-11 11:25:34 +0000

    initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+       <title>LESS</title> 
+       <link rel="stylesheet/less"  type="text/css" href="./style.less"> 
+       <script src="./less.js/dist/less.js"></script> 
+    </head>
+    <body>
+        <h3>LESS tutorial</h3>
+
+        <ul>
+            <li><a href="intro_less.php">Introductie en setup Less</a></li>
+            <li><a href="table_less.php">Tabel opmaak met Less</a></li>            
+        </ul>
+    </body>
+</html>
